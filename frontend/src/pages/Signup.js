6c9812233baa24c1a590814179c74e44255e1d57
Anton Severino <antonseverino6@gmail.com>
2023-04-11 18:39:51 +0800

    first commit

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Col, Container, Form, Row, Button } from "react-bootstrap";
+import { useSignupUserMutation } from "../services/appApi";
+import { Link, useNavigate } from "react-router-dom";
+import "./Signup.css";
+
+function Signup() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [signupUser, { isLoading, error }] = useSignupUserMutation();
+    const navigate = useNavigate();
+
+    async function handleSignup(e) {
+        e.preventDefault();
+        
+        signupUser({ name, email, password}).then(({ data }) => {
+            if (data) {
+                console.log(data);
+                navigate("/chat");
+            }
+        });
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col md={12} className="d-flex align-items-center justify-content-center flex-direction-column mt-5">
+                    <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleSignup}>
+                        <h1 className="text-center">Create account</h1>
+                        {error && <p className="alert alert-danger">{error.data}</p>}
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Your name" onChange={(e) => setName(e.target.value)} value={name} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            { isLoading ? "Signing you up..." : "Signup"}
+                        </Button>
+                        <div className="py-4">
+                            <p className="text-center">
+                                Already have an account ? <Link to="/">Login</Link>
+                            </p>
+                        </div>
+                    </Form>
+                </Col>
+                {/* <Col md={5} className="signup__bg"></Col> */}
+            </Row>
+        </Container>
+    );
+}
+
+export default Signup;
