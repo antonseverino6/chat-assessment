15ea55a05e698bba51591c840600bc99ed0c3de1
antonseverino6 <54117618+antonseverino6@users.noreply.github.com>
2023-04-20 05:55:06 +0800

    Delete backend directory

--- backend/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(`mongodb+srv://mernchat-yt-user:8pagMfiOdbGOYXgs@cluster0.z8f6zer.mongodb.net/?retryWrites=true&w=majority`, ()=> {
-  console.log('connected to mongodb')
-})
